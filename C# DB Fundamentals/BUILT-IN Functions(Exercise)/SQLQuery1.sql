8f7beb6d55785b7373116b5c9d53604b1c478fc3
djengo16 <53972425+djengo16@users.noreply.github.com>
2020-06-12 22:50:51 +0300

    update

--- C# DB Fundamentals/BUILT-IN Functions(Exercise)/SQLQuery1.sql
+++ C# DB Fundamentals/BUILT-IN Functions(Exercise)/SQLQuery1.sql
@@ -1,81 +1,161 @@
 USE SoftUni
 
+--Problem 1.	Find Names of All Employees by First Name
 SELECT FirstName,LastName
 FROM Employees
 Where FirstName LIKE 'SA%'
 
+--2.Find Names of All employees by Last Name 
 SELECT FirstName,LastName 
 FROM Employees
 WHERE LastName LIKE '%ei%'
+
+--Problem 3.	Find First Names of All Employees
+--Write a SQL query to find the first names of all employees in the departments with ID 3 or 10
+--and whose hire year is between 1995 and 2005 inclusive.
 
 SELECT FirstName
 FROM Employees
 WHERE DepartmentID IN(3,10) 
 AND CAST(DATEPART(YEAR,HireDate)AS INT) BETWEEN 1995 AND 2005
 
+
+--Problem 4.	Find All Employees Except Engineers
+--Write a SQL query to find the first and last names of all employees whose job titles does not contain “engineer”. 
+
+
 SELECT FirstName,LastName
 FROM Employees
 WHERE JobTitle NOT LIKE '%engineer%'
+
+--Problem 5.	Find Towns with Name Length
+--Write a SQL query to find town names that are 5 or 6 symbols long and order them alphabetically by town name. 
+
 
 SELECT [Name] 
 FROM Towns
 Where LEN([Name]) IN (5,6)
 ORDER BY [Name] ASC
 
-SELECT TownID,[Name]
-FROM Towns
+--Find Towns Starting With
+--Write a SQL query to find all towns that start with letters M, K, B or E.
+--Order them alphabetically by town name. 
+
+SELECT * FROM Towns
 WHERE [Name] LIKE '[MKBE]%'
-ORDER BY [Name]
+ORDER BY [Name] ASC
 
-SELECT TownID,[Name]
-FROM Towns
+--Problem 7.	 Find Towns Not Starting With
+--Write a SQL query to find all towns that does not start with letters R, B or D.
+--Order them alphabetically by name. 
+
+SELECT * FROM Towns
 WHERE [Name] NOT LIKE '[RBD]%'
-ORDER BY [Name]
+ORDER BY [Name] ASC
 
-CREATE VIEW V_EmployeesHiredAfter2000 AS
+
+--Problem 8.	Create View Employees Hired After 2000 Year
+--Write a SQL query to create view V_EmployeesHiredAfter2000
+--with first and last name to all employees hired after 2000 year. 
+
+CREATE View V_EmployeesHiredAfter2000
+AS 
+SELECT FirstName,LastName FROM Employees
+WHERE CONVERT(INT,DATEPART(year,HireDate)) > 2000
+
+--Problem 9.	Length of Last Name
+--Write a SQL query to find the names of all employees whose last name is exactly 5 characters long.
+
 SELECT FirstName,LastName
-FROM Employees
-WHERE DATEPART(YEAR,HireDate) > 2000
+  FROM Employees
+  WHERE LEN(LastName) = 5
 
-SELECT * FROM V_EmployeesHiredAfter2000
 
-SELECT FirstName,LastName
-FROM Employees
-WHERE LEN(LastName) = 5
 
+--Problem 10. Rank Employees by Salary
+
+SELECT * FROM (
 SELECT EmployeeID,FirstName,LastName,Salary,
-DENSE_RANK() OVER(PARTITION BY Salary ORDER BY EmployeeID) AS [Rank]
-FROM Employees
-WHERE Salary BETWEEN 10000 AND 50000 
-ORDER BY Salary DESC
+ DENSE_RANK() OVER(PARTITION BY Salary ORDER BY EmployeeID) AS [Rank]
+ FROM Employees
+ WHERE Salary BETWEEN 10000 AND 50000 
+ ) AS temp
+ WHERE Rank = 2
+ ORDER BY Salary DESC
 
-SELECT * FROM (SELECT EmployeeID,FirstName,LastName,Salary,
-DENSE_RANK() OVER(PARTITION BY Salary ORDER BY EmployeeID) AS [Rank]
-FROM Employees
-WHERE Salary BETWEEN 10000 AND 50000 ) AS temp
-WHERE temp.Rank = 2
-ORDER BY temp.Salary DESC
 
-USE [Geography]
+ --Problem 12.	Countries Holding ‘A’ 3 or More Times
+ USE Geography
 
-SELECT CountryName,IsoCode 
-FROM Countries
-WHERE CountryName LIKE '%a%a%a%'
-ORDER BY IsoCode
+ SELECT CountryName,IsoCode
+ FROM Countries
+ WHERE CountryName LIKE '%a%a%a%'
+ ORDER BY IsoCode
 
-SELECT p.PeakName,r.RiverName,LOWER(CONCAT(LEFT(p.PeakName,LEN(p.PeakName)-1),r.RiverName)) AS [Mix]
-FROM Peaks AS p, Rivers as r
-WHERE RIGHT(p.PeakName,1) = LEFT(r.RiverName,1)
-ORDER BY [Mix]
+ --Problem 13.	 Mix of Peak and River Names
+ SELECT  p.PeakName,
+		 r.RiverName,
+		 LOWER(CONCAT(LEFT(p.PeakName,LEN(p.PeakName) - 1),r.RiverName)) AS [Mix]
+         FROM
+         Peaks As p,Rivers AS r
+   WHERE Right(p.PeakName,1) = Left(r.RiverName,1)
+   ORDER BY Mix
+
+
+
+--Problem 14.	Games from 2011 and 2012 year
+--Find the top 50 games ordered by start date, then by name of the game. 
+--Display only games from 2011 and 2012 year. Display start date in the format “yyyy-MM-dd”. 
 
 USE Diablo
 
-SELECT TOP(50)[Name],FORMAT ([Start], 'yyyy-MM-dd') as [Start]
-FROM Games
-WHERE DATEPART(YEAR,[Start]) IN (2011,2012)
-ORDER BY [Start]
+SELECT TOP(50)Name,FORMAT(Start,'yyyy-MM-dd') AS [Start]
+       FROM Games
+	   WHERE DATEPART(year,start) IN(2011,2012) 
+	   ORDER BY Start,Name
 
-SELECT Username, SUBSTRING(Email,CHARINDEX('@',Email,1)+1,LEN(Email)) 
-AS 'Email Provider' 
+--Problem 15.  User Email Providers
+--Find all users along with information about their email providers. 
+--Display the username and email provider. Sort the results by email provider alphabetically, then by username. 
+
+SELECT Username,SUBSTRING(Email,CHARINDEX('@',Email,1)+1,LEN(Email))
+AS [Email Provider]
 FROM Users
-ORDER BY [Email Provider], Username
+ORDER BY [Email Provider],Username
+
+--Problem 16.	 Get Users with IPAdress Like Pattern
+--Find all users along with their IP addresses sorted by username alphabetically.
+--Display only rows that IP address matches the pattern: “***.1^.^.***”. 
+--Legend: * - one symbol, ^ - one or more symbols
+
+SELECT Username,IpAddress AS [IP Adress]
+FROM   Users
+WHERE  IpAddress LIKE '___.1%.%.___'
+ORDER  BY [Username] ASC
+
+
+
+
+--Problem 17. Show All Games with Duration and Part of the Day
+
+
+  SELECT G.Name,
+    CASE
+        WHEN (DATEPART(HOUR, G.Start)) BETWEEN 0 AND 11 THEN 'Morning'
+        WHEN (DATEPART(HOUR, G.Start)) BETWEEN 12 AND 17 THEN 'Afternoon'
+        WHEN (DATEPART(HOUR, G.Start)) BETWEEN 18 AND 23 THEN 'Evening'
+    END AS [Part of Day],
+    CASE
+        WHEN G.Duration <= 3 THEN 'Extra Short'
+        WHEN G.Duration BETWEEN 4 AND 6 THEN 'Short'
+        WHEN G.Duration IS NULL THEN 'Extra Long'
+        ELSE 'Long'
+    END AS [Duration]
+FROM Games AS G
+ORDER BY G.Name ASC,
+    [Duration],
+    [Part of Day]
+
+--Problem 18.	 Orders Table
+	USE [Online Store]
+	SELECT * FROM Orders
--- C# DB Fundamentals/Subqueries and JOINs/SQLQuery2.sql
+++ C# DB Fundamentals/Subqueries and JOINs/SQLQuery2.sql
@@ -26,6 +26,7 @@ ORDER BY e.EmployeeID
 
 --Problem 04.
 
+
 SELECT TOP(5)e.EmployeeID,e.FirstName,e.Salary,d.Name AS DepartmentName
 FROM Employees AS e
 JOIN Departments AS d 
@@ -68,6 +69,7 @@ WHERE e.EmployeeID = 24
 
 --Problem 09.
 
+
 SELECT e.EmployeeID,e.FirstName,e.ManagerID,m.FirstName 
 FROM Employees AS e
 JOIN Employees AS m ON e.ManagerID = m.EmployeeID
@@ -75,7 +77,6 @@ WHERE e.ManagerID IN (3,7)
 ORDER BY e.EmployeeID
 
 --Problem 10.
-
 
 SELECT TOP(50)
 	e.EmployeeID,
@@ -88,6 +89,7 @@ FROM Employees AS e
 ORDER BY e.EmployeeID
 
 --Problem 11.
+
 SELECT MIN(dt.AverageSalary) AS MinAverageSalary FROM
 (
 SELECT AVG(e.Salary) AS AverageSalary
@@ -111,6 +113,7 @@ ORDER BY p.Elevation DESC
 
 --Problem 13.
 
+
 SELECT mc.CountryCode,COUNT(m.MountainRange)
 FROM Mountains AS m
 JOIN MountainsCountries AS mc ON m.Id = mc.MountainId
@@ -127,7 +130,16 @@ WHERE c.ContinentCode = 'AF'
 ORDER BY c.CountryName ASC
 
 -- Problem 15.
-
+SELECT ContinentCode,CurrencyCode,CurrencyCount1 AS CurrencyUsage FROM 
+(
+SELECT *,DENSE_RANK() OVER (PARTITION BY ContinentCode ORDER BY CurrencyCount1 DESC) AS CurrencyCount FROM
+(
+SELECT ContinentCode,CurrencyCode,COUNT(*)AS CurrencyCount1 FROM Countries 
+GROUP BY ContinentCode,CurrencyCode 
+) AS tbl
+) AS tbl1
+WHERE CurrencyCount = 1 AND CurrencyCount1 != 1
+ORDER BY ContinentCode,CurrencyCode
 
 --Problem 16.
 
@@ -140,3 +152,54 @@ FROM MountainsCountries AS mc
 
 
 SELECT * FROM Rivers
+
+--Problem 17.
+
+--For each country, find the elevation of the highest peak and the length of the longest river,
+--sorted by the highest peak elevation (from highest to lowest), then by the longest river length 
+--(from longest to smallest), then by country name (alphabetically).
+--Display NULL when no data is available in some of the columns. Limit only the first 5 rows.
+
+SELECT TOP(5)  c.CountryName,MAX(p.Elevation) AS [HighestPeakElevation],MAX(r.Length) AS [LongestRiverLength]
+  FROM Countries AS c
+  FULL OUTER JOIN MountainsCountries AS mc ON c.CountryCode = mc.CountryCode
+  FULL OUTER JOIN Mountains AS m ON mc.MountainId = m.Id
+  FULL OUTER JOIN Peaks AS p ON m.Id = p.MountainId
+  FULL OUTER JOIN CountriesRivers AS cr ON c.CountryCode = cr.CountryCode
+  FULL OUTER JOIN Rivers AS r ON cr.RiverId = r.Id
+GROUP BY c.CountryCode,c.CountryName
+ORDER BY HighestPeakElevation DESC,
+		 LongestRiverLength   DESC,
+		 c.CountryName        ASC
+
+
+--Problem 18.	* Highest Peak Name and Elevation by Country
+--For each country, find the name and elevation of the highest peak,
+--along with its mountain. When no peaks are available in some country,
+--display elevation 0, "(no highest peak)" as peak name and "(no mountain)" as mountain name.
+--When multiple peaks in some country have the same elevation, display all of them. 
+--Sort the results by country name alphabetically, then by highest peak name alphabetically. Limit only the first 5 rows.
+
+SELECT TOP(5) Country,
+	          IIF([Highest Peak Name] IS NULL,'(no highest peak)',[Highest Peak Name]) AS [Highest Peak Name],
+	          IIF([Highest Peak Elevation] IS NULL,'0',[Highest Peak Elevation]) AS [Highest Peak Elevation],
+	          IIF(Mountain IS NULL,'(no mountain)',Mountain)  AS [Mountain]
+	   FROM 
+(
+SELECT c.CountryName AS [Country],
+       p.PeakName AS [Highest Peak Name],
+	   p.Elevation AS [Highest Peak Elevation],
+	   ROW_NUMBER() OVER (PARTITION BY c.CountryCode ORDER BY p.Elevation) AS [HighestPeak],
+	   m.MountainRange AS [Mountain]
+FROM Countries AS c
+LEFT OUTER JOIN MountainsCountries AS mc ON c.CountryCode = mc.CountryCode
+LEFT OUTER JOIN Mountains AS m ON mc.MountainId = m.Id
+LEFT OUTER JOIN Peaks AS p ON mc.MountainId = p.MountainId
+GROUP BY c.CountryCode,c.CountryName,p.PeakName,p.Elevation,m.MountainRange
+) AS tmp
+WHERE HighestPeak = 1
+ORDER BY Country ASC,
+         [Highest Peak Name] ASC
+
+
+		 
