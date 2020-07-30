ef6dcf5e79ab44249a353423dfd3ee63b62af3ae
djengo16 <53972425+djengo16@users.noreply.github.com>
2020-07-30 15:56:47 +0300

    Create Models

---
+++ Entity Framework Core/Workshop/PetStore/PetStore.Models/Client.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PetStore.Models
+{
+    class Цлиент
+    {
+    }
+}
---
+++ Entity Framework Core/Workshop/PetStore/PetStore.Models/Pet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PetStore.Models
+{
+    public class Pet
+    {
+        public Pet()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+        [Key]
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
