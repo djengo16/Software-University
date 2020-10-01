3986951016b30c0c20083b6bce0221103239c8ea
djengo16 <53972425+djengo16@users.noreply.github.com>
2020-10-01 14:37:35 +0300

    Created functions and classes

--- C# Web Basics/SUS/MyFirstMvcApp/Program.cs
+++ C# Web Basics/SUS/MyFirstMvcApp/Program.cs
@@ -1,12 +1,57 @@
-﻿using System;
+﻿using SUS.HTTP;
+using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace MyFirstMvcApp
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var server = new HttpServer();
+
+            server.AddRoute("/", HomePage);
+            server.AddRoute("/favicon", Favicon);
+            server.AddRoute("/about", About);
+            server.AddRoute("/login", Login);
+
+            await server.StartAsync(80);
+        }
+
+        static HttpResponse HomePage(HttpRequest request)
+        {
+            var responseHtml = "<h1>Welcome!</h1>" +
+                request.Headers.FirstOrDefault(x => x.Name == "User-Agent")?.Value;
+            var responseBodyBytes = Encoding.UTF8.GetBytes(responseHtml);
+            var response = new HttpResponse("text/html", responseBodyBytes);
+            return response;
+        }
+
+        static HttpResponse Favicon(HttpRequest request)
+        {
+            var fileBytes = File.ReadAllBytes("wwwroot/favicon.ico");
+            var response = new HttpResponse("image/vnd.microsoft.icon", fileBytes);
+            return response;
+        }
+
+        static HttpResponse About(HttpRequest request)
+        {
+            var responseHtml = "<h1>About...</h1>";
+            var responseBodyBytes = Encoding.UTF8.GetBytes(responseHtml);
+            var response = new HttpResponse("text/html", responseBodyBytes);
+            return response;
+        }
+
+        static HttpResponse Login(HttpRequest request)
+        {
+            var responseHtml = "<h1>Login...</h1>";
+            var responseBodyBytes = Encoding.UTF8.GetBytes(responseHtml);
+            var response = new HttpResponse("text/html", responseBodyBytes);
+            return response;
         }
     }
 }
---
+++ C# Web Basics/SUS/SUS.HTTP/HttpResponse.cs
@@ -0,0 +1,55 @@
+﻿namespace SUS.HTTP
+{
+    using System;
+    using System.Net;
+    using System.Text;
+    using System.Collections.Generic;
+
+
+    public class HttpResponse
+    {
+        public HttpResponse(string contentType, byte[] body, HttpStatusCode statusCode = HttpStatusCode.OK)
+        {
+            if (body == null)
+            {
+                throw new ArgumentNullException(nameof(body));
+            }
+
+            this.StatusCode = statusCode;
+            this.Body = body;
+            this.Headers = new List<Header>
+            {
+                { new Header("Content-Type", contentType) },
+                { new Header("Content-Length", body.Length.ToString()) },
+            };
+            this.Cookies = new List<Cookie>();
+        }
+
+        public override string ToString()
+        {
+            StringBuilder responseBuilder = new StringBuilder();
+            responseBuilder.Append($"HTTP/1.1 {(int)this.StatusCode} {this.StatusCode}" + HttpConstants.NewLine);
+            foreach (var header in this.Headers)
+            {
+                responseBuilder.Append(header.ToString() + HttpConstants.NewLine);
+            }
+
+            foreach (var cookie in this.Cookies)
+            {
+                responseBuilder.Append("Set-Cookie: " + cookie.ToString() + HttpConstants.NewLine);
+            }
+
+            responseBuilder.Append(HttpConstants.NewLine);
+
+            return responseBuilder.ToString();
+        }
+
+        public HttpStatusCode StatusCode { get; set; }
+
+        public ICollection<Header> Headers { get; set; }
+
+        public ICollection<Cookie> Cookies { get; set; }
+
+        public byte[] Body { get; set; }
+    }
+}
