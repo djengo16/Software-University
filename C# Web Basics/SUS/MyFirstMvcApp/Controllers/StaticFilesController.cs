656953996fa3bffa9a5d9f8f14f920a76dbe08e2
djengo16 <53972425+djengo16@users.noreply.github.com>
2020-10-03 19:44:50 +0300

    add bootstrap, js ,css

--- C# Web Basics/SUS/MyFirstMvcApp/Controllers/StaticFilesController.cs
+++ C# Web Basics/SUS/MyFirstMvcApp/Controllers/StaticFilesController.cs
@@ -9,11 +9,29 @@ namespace MyFirstMvcApp.Controllers
 {
     public class StaticFilesController : Controller
     {
-         public HttpResponse Favicon(HttpRequest request)
+        public HttpResponse Favicon(HttpRequest request)
         {
-            var path ="../../../wwwroot/favicon.ico";
-            var contentType ="image/x-icon";
-            return this.File(path,contentType);
+            return this.File("wwwroot/favicon.ico", "image/vnd.microsoft.icon");
+        }
+
+        internal HttpResponse BootstrapCss(HttpRequest request)
+        {
+            return this.File("wwwroot/css/bootstrap.min.css", "text/css");
+        }
+
+        internal HttpResponse CustomCss(HttpRequest request)
+        {
+            return this.File("wwwroot/css/custom.css", "text/css");
+        }
+
+        internal HttpResponse CustomJs(HttpRequest request)
+        {
+            return this.File("wwwroot/js/custom.js", "text/javascript");
+        }
+
+        internal HttpResponse BootstrapJs(HttpRequest request)
+        {
+            return this.File("wwwroot/js/bootstrap.bundle.min.js", "text/javascript");
         }
     }
 }
