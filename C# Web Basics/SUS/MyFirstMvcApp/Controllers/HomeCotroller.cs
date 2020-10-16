7284bce16df86694a4b637fe9eddef9fed5d9cc9
djengo16 <53972425+djengo16@users.noreply.github.com>
2020-10-16 14:08:44 +0300

    fix

--- C# Web Basics/SUS/MyFirstMvcApp/Controllers/HomeCotroller.cs
+++ C# Web Basics/SUS/MyFirstMvcApp/Controllers/HomeCotroller.cs
@@ -1,10 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using SUS.HTTP;
+using SUS.MvcFramework;
 
 namespace BattleCards.Controllers
 {
-    class HomeCotroller
+    public class HomeController : Controller
     {
+        [HttpGet("/")]
+        public HttpResponse Index()
+        {
+            if (this.IsUserSignedIn())
+            {
+                return this.Redirect("/Cards/All");
+            }
+
+            return this.View();
+        }
     }
 }
