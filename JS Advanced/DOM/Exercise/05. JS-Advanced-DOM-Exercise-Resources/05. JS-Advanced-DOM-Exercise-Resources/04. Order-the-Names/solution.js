874f595795fcfc8e70b6b64cc069c14d4627852e
djengo16 <53972425+djengo16@users.noreply.github.com>
2021-01-18 00:33:50 +0200

    update

--- JS Advanced/DOM/Exercise/05. JS-Advanced-DOM-Exercise-Resources/05. JS-Advanced-DOM-Exercise-Resources/04. Order-the-Names/solution.js
+++ JS Advanced/DOM/Exercise/05. JS-Advanced-DOM-Exercise-Resources/05. JS-Advanced-DOM-Exercise-Resources/04. Order-the-Names/solution.js
@@ -1,16 +1,21 @@
 function solve() {
   let buttonElement = document.getElementsByTagName("button")[0];
+
   buttonElement.addEventListener("click", (event) => {
-    let nameElement = document.getElementsByTagName("input")[0].value;
-    let letter = nameElement.slice(0, 1).toUpperCase();
-    let letterPosition = letter.charCodeAt(0) - 64;
-    let liElement = document.getElementsByTagName("li")[letterPosition - 1];
+    let nameElement = document.getElementsByTagName("input")[0];
+    let letter = nameElement.value.slice(0, 1).toUpperCase();
+    let letterPosition = letter.charCodeAt(0) - 65;
+    let liElement = document.getElementsByTagName("li")[letterPosition];
+
+    nameElementValue =
+      nameElement.value.slice(0, 1).toUpperCase() +
+      nameElement.value.slice(1, nameElement.value.length).toLowerCase();
+
     if (!liElement.textContent) {
-      liElement.textContent = nameElement;
-      nameElement.value = "";
+      liElement.textContent = nameElementValue;
     } else {
-      liElement.textContent += `, ${nameElement}`;
-      nameElement.value = "";
+      liElement.textContent += `, ${nameElementValue}`;
     }
+    nameElement.value = "";
   });
 }
--- JS Advanced/DOM/Exercise/05. JS-Advanced-DOM-Exercise-Resources/05. JS-Advanced-DOM-Exercise-Resources/05. Chat-Room/solution.js
+++ JS Advanced/DOM/Exercise/05. JS-Advanced-DOM-Exercise-Resources/05. JS-Advanced-DOM-Exercise-Resources/05. Chat-Room/solution.js
@@ -1,5 +1,13 @@
 function solve() {
-   //TODO...
+  document.getElementById("send").addEventListener("click", (ev) => {
+    let messageContent = document.querySelector("#chat_input");
+
+    let messageElement = document.createElement("div");
+    messageElement.setAttribute("class", "message my-message");
+    messageElement.textContent = messageContent.value;
+
+    document.getElementById("chat_messages").appendChild(messageElement);
+
+    messageContent.value = "";
+  });
 }
-
-
